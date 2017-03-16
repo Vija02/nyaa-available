5e30d3b3ff5dd54e0ace6de296d49a634f9227aa
Vija02 <salim_michaelmi@yahoo.com>
2017-03-16 17:54:24 +0000

    Create test for episode < 10

--- test/test.js
+++ test/test.js
@@ -8,8 +8,13 @@ describe('index', function() {
         chai.expect(res).to.be.false;
       });
     });
-    it('should return true when checking an existing episode', function() {
+    it('should return true when checking an existing episode (Last episode)', function() {
       nyaa.checkEpisode('No Game No Life', 12).then((res) => {
+        chai.expect(res).to.be.true;
+      });
+    });
+    it('should return true when checking an existing episode (Episode < 10)', function() {
+      nyaa.checkEpisode("K-On!", 3).then((res) => {
         chai.expect(res).to.be.true;
       });
     });
